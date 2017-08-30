00665669899e603f1aa9229746a1c1319202688f
Rafael García Doblas <rafamalaga86@gmail.com>
2017-08-30 11:00:12 +0200

    Settings for deployment

--- waip/settings.py
+++ waip/settings.py
@@ -130,5 +130,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'games/static/'),
+)
+
 # Overriding Python for getting DebugBar to work
 INTERNAL_IPS = ['127.0.0.1']
